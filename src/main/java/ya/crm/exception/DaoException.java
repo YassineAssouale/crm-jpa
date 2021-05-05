ca5d8ddf368d66dd491fa1d53d510a7f29e7c008
Yassine <yassine.assouale@gmail.com>
2021-05-05 12:07:41 +0200

    Adding Exception folder

---
+++ src/main/java/ya/crm/exception/DaoException.java
@@ -0,0 +1,27 @@
+package ya.crm.exception;
+
+public class DaoException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DaoException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DaoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DaoException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
